7b547a85a6b9f623056841a932e324a1b457c3f0
Andrea <andreaarcu@gmail.com>
2024-06-14 16:13:12 +0200

    context-exercise

--- src/AlertClock.jsx
+++ src/AlertClock.jsx
@@ -1,8 +1,18 @@
+import React, { useContext } from "react";
+import { LanguageContext } from "./LanguageContext";
+
+const messages = {
+  en: "Current time is",
+  it: "L'ora attuale è",
+};
+
 export default function AlertClock() {
+  const { language } = useContext(LanguageContext);
+
   function handleButtonClick() {
     const actualTime = new Date();
-
-    alert(`The current time is ${actualTime.toLocaleTimeString()}`);
+    alert(`${messages[language]} ${actualTime.toLocaleTimeString()}`);
   }
-  return <button onClick={handleButtonClick}>Showtime!</button>;
+
+  return <button onClick={handleButtonClick}>Show</button>;
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import "./App.css";
 import AlertClock from "./AlertClock";
+import { LanguageContext, LanguageProvider } from "./LanguageContext";
 
-function App() {
-  const [count, setCount] = useState(0);
+function LanguageSelector() {
+  const { language, setLanguage } = useContext(LanguageContext);
+
+  const handleLanguageChange = (event) => {
+    setLanguage(event.target.value);
+  };
 
   return (
-    <>
-      <div>
-        <AlertClock />
-      </div>
-    </>
+    <select value={language} onChange={handleLanguageChange}>
+      <option value="en">English</option>
+      <option value="it">Italiano</option>
+    </select>
+  );
+}
+
+function App() {
+  return (
+    <LanguageProvider>
+      <LanguageSelector />
+      <AlertClock />
+    </LanguageProvider>
   );
 }
 
